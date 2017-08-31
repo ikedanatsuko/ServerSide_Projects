8087ba0a0f7236e5015e50c81e5bad4e0824a587
utgwn <natsu950821@yahoo.co.jp>
2017-09-01 04:57:39 +0900

    ログイン・ログアウト時のトークン処理を実装

--- SpringBoot_Twitter_OAuth/src/main/java/io/github/oauth/controller/TwitterConnectController.java
+++ SpringBoot_Twitter_OAuth/src/main/java/io/github/oauth/controller/TwitterConnectController.java
@@ -2,23 +2,49 @@ package io.github.oauth.controller;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.github.oauth.token.TokenManager;
 
 @Controller
 @RequestMapping("/connect")
 public class TwitterConnectController extends ConnectController {
 	
+	@Autowired
+	private TokenManager tokenManager;
+	
+	private Twitter twitter;
+	
 	@Inject
-	public TwitterConnectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
+	public TwitterConnectController(Twitter twitter, ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
 		super(connectionFactoryLocator, connectionRepository);
+		this.twitter = twitter;
 	}
 	
 	@Override
 	protected String connectedView(String provideId) {
+		if (twitter.isAuthorized()) {
+			 String token = tokenManager.setToken(
+					 twitter.userOperations().getUserProfile().getScreenName());
+		}
 		return "redirect:/";
+	}
+	
+	@Override
+	@RequestMapping(value="/{providerId}", method=RequestMethod.DELETE)
+	public RedirectView removeConnections(@PathVariable String providerId, NativeWebRequest request) {
+		tokenManager.destroyToken(twitter.userOperations().getUserProfile().getScreenName());
+		
+		return super.removeConnections(providerId, request);
 	}
 }
