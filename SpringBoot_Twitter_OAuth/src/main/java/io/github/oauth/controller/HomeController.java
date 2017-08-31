f2e27a4e0e480a03e68bf1c2a2b11d72e0da0368
utgwn <natsu950821@yahoo.co.jp>
2017-09-01 04:58:48 +0900

    ログイン後にトークンを表示するようにした

--- SpringBoot_Twitter_OAuth/src/main/java/io/github/oauth/controller/HomeController.java
+++ SpringBoot_Twitter_OAuth/src/main/java/io/github/oauth/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -11,10 +12,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import io.github.oauth.token.TokenManager;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private TokenManager tokenManager;
 	
 	private Twitter twitter;
 	
@@ -30,15 +35,20 @@ public class HomeController {
 	public String login(Model model){
 		if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
 			return "redirect:/connect/twitter";
-		}	
+		}
+		
 		return "redirect:/home";
 	}
 	
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String home(Model model){
+		model.addAttribute("token", 
+				tokenManager.getToken(twitter.userOperations().getScreenName()));
+		
 		model.addAttribute(twitter.userOperations().getUserProfile());
 		List<Tweet> favorites = twitter.timelineOperations().getFavorites();
 		model.addAttribute("favorites", favorites);
+		
 		return "home";
 	}
 }
