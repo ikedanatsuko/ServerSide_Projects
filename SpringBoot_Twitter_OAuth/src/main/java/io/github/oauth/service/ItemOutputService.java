a0bc89706af4bc55b53608eb9934cf7bf62b5322
utgwn <natsu950821@yahoo.co.jp>
2017-10-03 15:44:43 +0900

    集計結果を操作するサービスクラスを実装

---
+++ SpringBoot_Twitter_OAuth/src/main/java/io/github/oauth/service/ItemOutputService.java
@@ -0,0 +1,64 @@
+package io.github.oauth.service;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.oauth.dao.table.CreatedItemDaoImpl;
+import io.github.oauth.dao.table.DateTotalDao;
+import io.github.oauth.dao.table.DeletedItemDaoImpl;
+import io.github.oauth.entity.DateTotal;
+import io.github.oauth.entity.ItemOutput;
+
+@Service
+public class ItemOutputService {
+	
+	@Autowired
+	private DateTotalDao dateTotalDao;
+	
+	@Autowired
+	private CreatedItemDaoImpl createdItemDao;
+	@Autowired
+	private DeletedItemDaoImpl deletedItemDao;
+	
+	public List<DateTotal> getAllDateTotal() {
+		return dateTotalDao.getAllDateTotal();
+	}
+	
+	public List<DateTotal> getDateTotalsByTerm(Date begin, Date end) {
+		return dateTotalDao.getDateTotalsByTerm(begin, end);
+	}
+	
+	public List<List<ItemOutput>> getAllCreatedItem() {
+		return getCreatedItems(getAllDateTotal());
+	}
+	
+	public List<List<ItemOutput>> getAllDeletedItem() {	
+		return getDeletedItems(getAllDateTotal());
+	}
+	
+	public List<List<ItemOutput>> getCreatedItems(List<DateTotal> dateTotals) {
+		List<List<ItemOutput>> createdItems = new ArrayList<>();
+		
+		for (int i = 0; i < dateTotals.size(); i++) {
+			int dateId = dateTotals.get(i).getId();		
+			List<ItemOutput> tmpCreatedItems = createdItemDao.getItemsByDateId(dateId);
+			createdItems.add(tmpCreatedItems);
+		}		
+		return createdItems;
+	}
+	
+	public List<List<ItemOutput>> getDeletedItems(List<DateTotal> dateTotals) {
+		List<List<ItemOutput>> deletedItems = new ArrayList<>();
+		
+		for (int i = 0; i < dateTotals.size(); i++) {
+			int dateId = dateTotals.get(i).getId();
+			List<ItemOutput> tmpDeletedItems = deletedItemDao.getItemsByDateId(dateId);
+			deletedItems.add(tmpDeletedItems);
+		}
+		return deletedItems;
+	}
+}
