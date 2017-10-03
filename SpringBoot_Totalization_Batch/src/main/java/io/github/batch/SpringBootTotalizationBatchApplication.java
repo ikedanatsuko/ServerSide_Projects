0d62060607d015b63bb43157b2663630939c8643
utgwn <natsu950821@yahoo.co.jp>
2017-10-03 15:38:31 +0900

    Add main class

---
+++ SpringBoot_Totalization_Batch/src/main/java/io/github/batch/SpringBootTotalizationBatchApplication.java
@@ -0,0 +1,12 @@
+package io.github.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTotalizationBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTotalizationBatchApplication.class, args);
+	}
+}
